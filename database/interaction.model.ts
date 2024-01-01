8f4a80a3ee67881ec1f358929968c65dcd27ec3a
tazheeb-shamsi <thshamsi@hotmail.com>
2024-01-01 16:26:50 +0530

    [BUG]:#12 - Fixed bug while  attempting to create a new question.

--- database/interaction.model.ts
+++ database/interaction.model.ts
@@ -4,7 +4,7 @@ export interface IInteraction extends Document {
   user: Schema.Types.ObjectId;
   question: Schema.Types.ObjectId;
   answer: Schema.Types.ObjectId;
-  tags: Schema.Types.ObjectId;
+  tags: Schema.Types.ObjectId[];
   action: string;
   createdAt: Date;
 }
@@ -13,7 +13,7 @@ const InteractionSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   question: { type: Schema.Types.ObjectId, ref: "Question" },
   answer: { type: Schema.Types.ObjectId, ref: "Answer" },
-  tags: { type: Schema.Types.ObjectId, ref: "Tags" },
+  tags: [{ type: Schema.Types.ObjectId, ref: "Tags" }],
   action: { type: String, require: true },
   createdAt: { type: Date, default: Date.now },
 });
